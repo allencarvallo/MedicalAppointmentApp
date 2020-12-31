b982c4bec0134ae1fc7d48b26871bfbb15c70c0f
Allen Carvallo <crvlhallu@gmail.com>
2020-12-31 16:09:54 +0530

    Validating pating login  with storing patient id on success and throwing alert message on failure implemented

--- src/app/components/patient-login/patient-login.component.ts
+++ src/app/components/patient-login/patient-login.component.ts
@@ -9,20 +9,40 @@ import { Patient } from './../../models/patient';
 })
 export class PatientLoginComponent implements OnInit {
 
+  patientId?: any;
   patientEmail?: string;
   patientPassword?: string; 
 
+  loading: boolean = true;
+  errorMessage: string = "";
   constructor(private patientService: PatientService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
+
     const patientLoginData = {
       patientEmail: this.patientEmail,
       patientPassword: this.patientPassword
     }
-    // this.patientService.patientLogin(patientLoginData).subscribe();
+
+    this.patientService.patientLogin(patientLoginData)
+    .subscribe
+      (p => {
+        this.patientId = p.patientId;
+        console.log(this.patientId);
+        // To set patient Id in local storage
+        localStorage.setItem('patientId', this.patientId)
+        },
+      error => {                              
+        console.error('error caught in component')
+        this.errorMessage = error;
+        this.loading = false;
+        alert(`Invalid Credentials!! \nUser with email: ${patientLoginData.patientEmail} and password: ${patientLoginData.patientPassword} does not exist`);
+        }
+      );
+
   }
 
 }
