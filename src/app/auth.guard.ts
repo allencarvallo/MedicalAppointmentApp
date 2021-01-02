9b3d5df21f895b891fd25648f9372645198d6151
Allen Carvallo <crvlhallu@gmail.com>
2021-01-02 18:35:34 +0530

    added authguard to control access by checking if logged in or not

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,28 @@
+import { PatientService } from './services/patient.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private patientService: PatientService,
+              private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const patientId = localStorage.getItem('patientId');
+    if(patientId){
+      this.patientService.patientId= +patientId; 
+      return true;
+    }
+    else {
+      this.router.navigate(["patient-login"]);
+      return false;
+    }
+
+  }
+  
+}
