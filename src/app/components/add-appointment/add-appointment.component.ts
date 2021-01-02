0fe736d1543a1bd48e2703efce926dcb80e6dbed
Allen Carvallo <crvlhallu@gmail.com>
2021-01-02 18:37:42 +0530

    added routing

--- src/app/components/add-appointment/add-appointment.component.ts
+++ src/app/components/add-appointment/add-appointment.component.ts
@@ -6,6 +6,8 @@ import { Patient } from './../../models/patient';
 import { PatientService } from './../../services/patient.service';
 import { AppointmentService } from './../../services/appointment.service';
 import { Appointment } from './../../models/appointment';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-appointment',
@@ -22,15 +24,33 @@ export class AddAppointmentComponent implements OnInit {
   description?: string;
   doctorName?: string;
   doctorId?: number;
-  appointmentTime?: Date;
-  appointment: any;
-
+  appointmentTime?: any;
+  appointment?: Appointment;
+  appointmentId?: number;
+  
   constructor(private doctorService: DoctorService, 
               private patientService: PatientService,
               private appointmentService: AppointmentService,
+              private route: ActivatedRoute,
+              private router: Router
              ) {}
 
   ngOnInit(): void {
+
+    this.patientId = this.patientService.patientId;
+    const appointmentId = this.route.snapshot.paramMap.get('id');
+    if(appointmentId)
+    {
+      this.appointmentId = +appointmentId;
+    
+      this.appointmentService.getAppointmentById( this.appointmentId)
+        .subscribe(appointment => {
+          this.description = appointment.description;
+          this.appointmentTime = new DatePipe('en').transform(appointment.appointmentTime, 'yyyy-MM-dd');
+          this.doctorId = appointment.doctorId;
+        });
+        
+    }
 
     this.doctorService.getDoctors().subscribe(doctors => {
       this.doctors = doctors;
@@ -41,17 +61,24 @@ export class AddAppointmentComponent implements OnInit {
       this.patients = patients;
     });
 
-
   }
 
   onSubmit() {
-    const appointment = {
+    const appointment: Appointment = {
       description: this.description,
       appointmentTime: this.appointmentTime,
       doctorId: this.doctorId,
       patientId: this.patientId
+    };
+    if (this.appointmentId) {
+      appointment.appointmentId = this.appointmentId;
+      this.appointmentService.updateAppointment(appointment).subscribe(() =>
+        this.router.navigate(["appointments"])
+      );
     }
-    this.appointmentService.addAppointment(appointment).subscribe();
+    else {
+      this.appointmentService.addAppointment(appointment).subscribe();
+    }
   }
 
 }
