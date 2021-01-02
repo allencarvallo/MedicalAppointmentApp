4ec15ce9f95643cbab7d46396e9871b3ec6b1a9a
Allen Carvallo <crvlhallu@gmail.com>
2021-01-02 18:33:43 +0530

    Added authguards and edited routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { AddAppointmentComponent } from './components/add-appointment/add-appointment.component';
 
 import { AppointmentsComponent } from './components/appointments/appointments.component';
-import { AboutComponent } from './components/pages/about/about.component';
 import { PatientLoginComponent } from './components/patient-login/patient-login.component';
 import { PatientSignupComponent } from './components/patient-signup/patient-signup.component';
 
 const routes: Routes = [
   {
-    path: '', component: AppointmentsComponent
+    path: 'appointments', component: AppointmentsComponent, canActivate: [AuthGuard]
   },
   {
-    path: 'about', component: AboutComponent
+    path: 'add-appointment', component: AddAppointmentComponent, canActivate: [AuthGuard]
   },
   {
-    path: 'add', component: AddAppointmentComponent
+    path: 'edit-appointment/:id', component: AddAppointmentComponent, canActivate: [AuthGuard]
   },
   {
     path: 'patient-login', component: PatientLoginComponent
   },
   {
     path: 'patient-signup', component: PatientSignupComponent
+  },
+  { 
+    path: '',   redirectTo: 'appointments', pathMatch: 'full'
   }
 ];
 
